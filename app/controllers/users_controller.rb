e551991e98762e14ff0b9f8fb7cc770786cb8c24
Clau <claudiomrussi@hotmail.com>
2016-05-05 01:17:56 -0300

    Finishing user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user=User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success]="Welcome to the sample app"
+      redirect_to @user
+    else
+      render 'new'
+    end
+
+  end
+
+  private #Definimos los strong-params para poder elegir que parametros recibe del form, asi no pueden insertar paremtros no existentes.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
